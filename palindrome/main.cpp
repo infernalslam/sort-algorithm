faf969c9d3b3fb9e4f0e3975f1d112c98bfd3cca
infernalslam <infernal-slam@hotmail.com>
2016-12-03 03:45:05 +0700

    pal

---
+++ palindrome/main.cpp
@@ -0,0 +1,46 @@
+﻿#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+bool palindrome ();
+char text[1][50];
+
+void main () {
+	//char text[1][50];
+
+	int count = 0;
+	
+	cout<<"Enter text : ";
+	cin>>text[0];
+
+	// bool test = palindrome();
+
+	for (int i=0; i < 50; i++) {
+		if (text[0][i] == NULL) i=100; // not loop
+		else  count++; 
+	}
+
+	int check = count;
+	bool checkPal = false;
+
+	for (int r=0; r < count/2; r++) {
+		cout<<text[0][r]<<" = " << text[0][check-1]<<endl;
+
+		if(text[0][r] == text[0][check-1]) { checkPal = true; }
+		else if (text[0][r] != text[0][check-1]) { checkPal= false;  break; }
+
+		check -= 1;
+	}
+
+	if (checkPal == true && count%2 == 0) cout<<"Your text is palindrome .";
+	else if (checkPal == false) cout<<"Your text is not palindrome";
+	else if (checkPal == true) cout<<"Your text is not palindrome";
+
+	cout<<endl;
+}
+
+bool palindrome (char text[]) {
+	cout<<text[0]<<endl;
+	return true;
+}
